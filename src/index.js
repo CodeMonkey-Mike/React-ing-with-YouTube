0a4e527a5b39ecc2aae787ad3597887b4dfa48eb
Mike <mneder@spamarrest.com>
2017-04-05 12:17:22 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React, {Component} from 'react';
+import ReactDom from 'react-dom';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+//youtube API key
+const API_KEY = 'AIzaSyCZPAUzgfhG58zwsdt8fQkL1rkZAIjycAw';
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+//create a component
+
+class App extends Component{
+	constructor(props){
+		super(props);
+
+		this.state = {videos: []};
+
+			YTSearch({key: API_KEY, term: 'CodeMonkey'}, (videos) => {
+				this.setState({videos});
+
+		});
+
+	}
+
+	render() {
+		return (
+			 <div>
+				 <SearchBar/>
+			 </div>
+		);
+	}
+}
+
+
+//put component's HTML in the DOM
+
+ReactDom.render(<App/>, document.querySelector('.container'));
